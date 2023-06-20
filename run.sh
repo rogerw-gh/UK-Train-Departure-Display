b0cababa6f7bd7c41c10af5eb02da4a0f1ab6271
rogerw-gh <rogerw-gh@users.noreply.github.com>
2023-06-20 13:59:14 +0100

    Comment out --rotate 2 if display is right way up.

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 ./src/main.py --display ssd1322 --width 256 --height 64 --interface spi --mode 1 --rotate 2
+python3 ./src/main.py --display ssd1322 --width 256 --height 64 --interface spi --mode 1 
+# put rotate back into start line if your display is mounted upside down with the SPI
+# interface on the left. The platform based station display doesn't need the rotation
+# --rotate 2
